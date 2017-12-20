78cca185bf5268c462fd5948dc00ca8f2fee8131
Donald Nie <qidong.nie@leidos.com>
2017-12-20 19:30:40 +0000

    Some Changes

--- CommonLibGenerator.py
+++ CommonLibGenerator.py
@@ -1,3 +1,6 @@
+# EXPANDED_COPYRIGHT_TEXT_START: TFDM
+# (c) 2017 Leidos. All rights reserved.
+# EXPANDED_COPYRIGHT_TEXT_STOP: TFDM
 '''
 Created on Dec 14, 2017
 
@@ -9,24 +12,20 @@ from xml.etree.ElementTree import ElementTree, Element, SubElement, Comment, tos
 from xml.etree import ElementTree
 from xml.dom import minidom
 
-
-
-class CommonLibGenerator:        
+class CommonLibGenerator:
     def buildTree(self, imgLib):
         xmlFile = ET.Element("Resource")
         headerComment = ET.Comment("This is a generated xml file for Common Image library for String Testing")
-        
-        
-        name = ET.SubElement(xmlFile, "Common")
+        name = ET.SubElement(xmlFile, "Name")
         name.text = "Common"
-        
+
         resourcePath = ET.SubElement(xmlFile, "ResourcePath")
         resourcePath.text = "../../testlibs/CommonImage"
         xmlFile.append(headerComment)
         properties = ET.SubElement(xmlFile,"Properties")
         imageDir = {"Misc":[]}
         for img in imgLib:
-            imageName = img.rstrip(".png")
+            imageName = img[0:-4]
             if imageName.find("_") is not -1:
                 section = imageName[0:imageName.find("_")]
                 if not imageDir.has_key(section):
@@ -35,39 +34,40 @@ class CommonLibGenerator:
                     imageDir[section].append(imageName)
             else:
                 imageDir["Misc"].append(imageName)
-                #sectionStart = Comment("************"+section+"************")
-                #sectionEnd = Comment("____________"+section+"____________")
-                
-        
+
+
         for key in imageDir:
-            sectionStart = ET.Comment("************"+key+"************")
-            sectionEnd = ET.Comment("____________"+key+"____________")
-            properties.append(sectionStart)
-            for i in imageDir[key]:
-                propertyImage = ET.SubElement(properties, "Property")
-                property_name = ET.SubElement(propertyImage, "Name")
-                property_name.text = i
-                property_path = ET.SubElement(propertyImage, "Path")
-                property_path.text = i +".png"
-            properties.append(sectionEnd)
-            if key is not imageDir.keys()[-1]:
+            if key is not "Misc":
+                sectionStart = ET.Comment("************"+key+"************")
+                sectionEnd = ET.Comment("____________"+key+"____________")
+                properties.append(sectionStart)
+                for i in imageDir[key]:
+                    propertyImage = ET.SubElement(properties, "Property")
+                    property_name = ET.SubElement(propertyImage, "Name")
+                    property_name.text = i
+                    property_path = ET.SubElement(propertyImage, "Path")
+                    property_path.text = i +".png"
+                properties.append(sectionEnd)
                 properties.append(ET.Comment("                           "))
-                
-        #tree = ElementTree.ElementTree(xmlFile)
-        #f = open("Test.xml","w+")
-        #tree.write(f,encoding="utf-8",xml_declaration=True)
+
+        sectionStart = ET.Comment("************Misc************")
+        sectionEnd = ET.Comment("____________Misc____________")
+        properties.append(sectionStart)
+        for i in imageDir["Misc"]:
+            propertyImage = ET.SubElement(properties, "Property")
+            property_name = ET.SubElement(propertyImage, "Name")
+            property_name.text = i
+            property_path = ET.SubElement(propertyImage, "Path")
+            property_path.text = i +".png"
+        properties.append(sectionEnd)
+
         xmlString = ET.tostring(xmlFile, encoding="utf-8", xml_declaration=True, pretty_print=True)
-        f = open("xmlInclude/CommonImage","w+")
+        f = open("xmlInclude/CommonImage.xml","w+")
         f.write(xmlString)
         return xmlString
-        
-    def prettify(self,elem):
-        rawString = ElementTree.tostring(elem, "utf-8",xml_declaration=True, pretty_print=True)
-        parsed = minidom.parseString(rawString)
-        return parsed.toprettyxml(indent="  ")
 
 if __name__ == '__main__':
     try:
         CommonLibGenerator().buildTree(os.listdir("./CommonImage"))
     except OSError:
-        print("CommonImage Dir cannot be found")
+        print("Please Check Path, Make sure Running in testlibs/")
